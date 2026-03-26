c2f1c39fc0d34c5e72db405182565d4bc568ad02
Alaeddine <alaxg2005@gmail.com>
2026-03-26 13:12:15 +0100

    chore : identified the common http types req & res

--- server/include/common.h
+++ server/include/common.h
@@ -4,12 +4,22 @@
 #define BUFF_SIZE 4096
 
 
+typedef struct 
+{
+    ;
+}http_request_t;
 
+typedef struct
+{
+    ;
+}http_response_t;
 
 typedef int8_t socket_fd_t;
+typedef ssize_t msg_len_t;
 typedef struct http_request http_request_t;
 typedef struct http_response http_response_t;
 typedef int8_t success_flag_t;
 typedef int16_t n_bytes_t;
+
 
 #endif
