4cf4c2eacad12b32f322806277ab2fef8fbc373d
Alaeddine <alaxg2005@gmail.com>
2026-03-26 13:13:06 +0100

    chore : set the skeleton for http calls handlers

--- server/include/handler.h
+++ server/include/handler.h
@@ -11,6 +11,7 @@
 
 // user-defined constants
 
-n_bytes_t handle_client(socket_fd_t fd);
+n_bytes_t http_handler(char* buffer);
+n_bytes_t handel_http_response();
 
 #endif
--- server/src/handler.c
+++ server/src/handler.c
@@ -1,27 +1,14 @@
 #include "../include/handler.h"
 
 
-n_bytes_t handle_client(socket_fd_t fd)
+n_bytes_t http_handler(char* buffer)
 {
-    char* buffer = malloc(BUFF_SIZE);
-    if(!buffer) {
-        perror("Error allocating memory\n");
-        close(fd);
-        return -1;
-    }
-    memset(buffer, 0, BUFF_SIZE);
     n_bytes_t bytes;
-    bytes = recv(fd, buffer, BUFF_SIZE, 0);
-    if (bytes <= 0) {
-        perror("Error allocating memory\n");
-        free(buffer);
-        close(fd);
-        return -1;
-    }
-    http_request_t* req;
+    http_request_t* req = serialize(buffer);
     success_flag_t i = http_request_handler();
-    http_response_t* res;
-    success_flag_t j = http_response_handler();
+    
+    http_response_t* res = http_response_handler();
+
     return bytes;
 }
 
