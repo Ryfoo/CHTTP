85128b5ce41105c271cfb63633994a7e78c20db0
Alaeddine <alaxg2005@gmail.com>
2026-03-27 22:47:28 +0100

    fix: types conventions

--- server/include/common.h
+++ server/include/common.h
@@ -1,14 +1,17 @@
 
-#ifndef TYPES_H
-#define TYPES_H
+#include <stdint.h>
+
+#ifndef COMMON_H
+#define COMMON_H
 
 
 #define FAILURE -1
 #define SUCCESS 0
 
 #define BUFF_SIZE 4096
-#define RECV_BUFFER_SIZE    8192
+#define RECV_BUFFER_SIZE 8192
 
+#define SHORT_LENGTH 32
 #define HEADERS_LEN 32
 #define HEADER_NAME_LENGTH 64
 #define HEADER_VALUE_LENGTH 512
@@ -16,43 +19,39 @@
 #define BODY_SIZE 8192  
 
 typedef int8_t socket_fd_t;
-typedef ssize_t msg_len_t;
+typedef size_t msg_len_t;
 typedef int8_t success_flag_t;
 typedef int16_t n_bytes_t;
 
 
 typedef struct {
-    char name[32];
-    char value[256];
-}header;
+    char name[HEADER_NAME_LENGTH];
+    char value[HEADER_VALUE_LENGTH];
+}header_t;
 
-typedef struct 
-{
-    struct request_line_t* req_line;
-    struct headers_t* headers;
-    char* body;
-    size_t body_size;
 
-}http_request_t;
 
 typedef struct
 {
-    char* method;
-    char* uri;
-    char* http_version;
+    char method[SHORT_LENGTH];
+    char uri[SHORT_LENGTH];
+    char http_version[SHORT_LENGTH];
 
 } request_line_t;
 
 typedef struct 
 {
-    header headers[HEADERS_LEN];
+    header_t headers[HEADERS_LEN];
     size_t headers_counter;
-} headers_t;
+} headers_list_t;
 
+typedef struct 
+{
+    request_line_t* req_line;
+    headers_list_t* head;
+    char* body;
+    size_t body_size;
 
-
-void http_request_init(http_request_t *req);
-void http_request_add_header(http_request_t *req, const char *name, const char *value);
-void http_request_set_body(http_request_t *req, const char *data, size_t len);
+}http_request_t;
 
 #endif
